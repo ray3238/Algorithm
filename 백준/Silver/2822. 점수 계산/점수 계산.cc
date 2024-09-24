1111c0ffd11651f5414daf90c5bfc7d4b4b31d27
이태규 <127665596+ray3238@users.noreply.github.com>
2024-09-24 10:07:33 +0900

    [Silver V] Title: 점수 계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> vec;
+    vector<int> result;
+    vector<int> result2;
+    int n;
+    int sum = 0;
+
+    for (int i = 0 ; i < 8 ; i++) {
+        cin >> n;
+        vec.push_back(n);
+        result.push_back(n);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    for (int i = 7; i > 2; i--) {
+        if(find(result.begin(), result.end(), vec[i]) != result.end()) { // vector내에 6이 존재함
+            sum += result[find(result.begin(), result.end(), vec[i]) - result.begin()];
+            result2.push_back(find(result.begin(), result.end(), vec[i]) - result.begin() + 1);
+            //cout << find(result.begin(), result.end(), vec[i]) - result.begin() + 1 << " ";  index 확인
+        }
+    }
+
+    sort(result2.begin(), result2.end());
+
+    cout << sum << '\n';
+    for (int i = 0 ; i < result2.size() ; i++) {
+        cout << result2[i] << " ";
+    }
+
+    return 0;
+}
